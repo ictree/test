061137e7026bbf2db2b8e5e7aab37c3cc482719c
System Administrator <root@wanghaiyangtekiMacBook-Pro.local>
2014-03-15 22:10:29 +0800

    init

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.oceanopencart.com/admin/');
+define('HTTP_CATALOG', 'http://www.oceanopencart.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.oceanopencart.com/admin/');
+define('HTTPS_CATALOG', 'http://www.oceanopencart.com/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/opencart/admin/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/opencart/system/');
+define('DIR_DATABASE', '/Library/WebServer/Documents/opencart/system/database/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/opencart/admin/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/opencart/admin/view/template/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/opencart/system/config/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/opencart/image/');
+define('DIR_CACHE', '/Library/WebServer/Documents/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/opencart/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/opencart/system/logs/');
+define('DIR_CATALOG', '/Library/WebServer/Documents/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.oceanopencart.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.oceanopencart.com/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/opencart/catalog/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/opencart/system/');
+define('DIR_DATABASE', '/Library/WebServer/Documents/opencart/system/database/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/opencart/system/config/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/opencart/image/');
+define('DIR_CACHE', '/Library/WebServer/Documents/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/opencart/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
